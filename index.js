007b51ce99c194ca274fd02106250c6faad18839
daniel <d_nni3l@protonmail.ch>
2020-04-16 16:28:37 -0700

    (1.2.4) Reorganized code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// Tweeter bot by cyckl
+// Tweeter bot by cyckl - Dedicated to Merijn 1998 - 2018
 
 // Call in dependencies / set up global variables
 const { Client, MessageEmbed } = require('discord.js');
 const client = new Client();
-const version = '1.2.3';
+const version = '1.2.4';
 var token = process.argv[2];
 
 // Bootup sequence
@@ -16,24 +16,23 @@ client.on('ready', () => {
     client.user.setActivity('.t msg');
 });
 
-// Command init
+// Command list
 client.on('message', (message) => {
     if (message.content.startsWith('.t ')) tweet(message);
-    if (message.content.startsWith('.run ')) exec(message);
     if (message.content.startsWith('.about')) about(message);
-});
+    if (message.content.startsWith('.run ')) run(message);
+})
 
-// Global Functions
-function getRandomInt(x) {
-    return Math.floor(Math.random() * x);
-}
-
-// Commands
+// Command logic
 function tweet(message) {
-    // Handles removing nickname if no nickname
+    //Random integer
+    function getRandomInt(x) {
+        return Math.floor(Math.random() * x);
+    }
+    // Nickname logic
     if (message.member.nickname == null) tweetUserString = message.author.username;
     else tweetUserString = message.member.nickname + ' (@' + message.author.username + ')';
-
+    // Tweet gen
     var content = message.content.replace('.t ', '');
     var embed = new MessageEmbed()
         .setAuthor(tweetUserString, message.author.displayAvatarURL(), 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
@@ -47,11 +46,25 @@ function tweet(message) {
     console.log('[Tweet] (' + message.author.tag + ') ' + content);
 }
 
-function exec(message) {
+function about(message) {
+    var embed = new MessageEmbed()
+        .setTitle('Tweeter by cyckl')
+        .setAuthor('About', 'https://github.com/cyckl/tweeter/raw/master/img/tweeter.png')
+        .setColor(0xFF453A)
+        .setDescription('Tweeter is a mock Twitter embed generator for Discord.')
+        .addField('Version', version, true)
+        .addField('Build date', '2020-04-16', true)
+        .addField('Github', 'https://github.com/cyckl/tweeter', false)
+        .setFooter('This is alpha software. Please be patient!')
+        .setThumbnail('https://github.com/cyckl/tweeter/raw/master/img/tweeter.png');
+    // Send tweet and log
+    message.channel.send(embed);
+    console.log('[About] (' + message.author.tag + ') ' + 'About dialogue triggered.');
+}
+
+function run(message) {
     // User ID hardcode
     if (message.author.id != '248948821881520138' && message.author.id != '249052912762880000') return;
-    
-    // Try code, if broken, move to catch()
     try {
         var runCode = message.content.replace('.run ', '');
         var result = eval(runCode);
@@ -60,27 +73,11 @@ function exec(message) {
         console.log('[Exec] (' + message.author.tag + ') ' + runCode);
         console.log('[Exec Result] ' + result);
     }
-    
-    // If error, handle it instead of crashing.
     catch (e) {
         console.log('[Exec Error] ' + e);
         message.channel.send('**Error:** ```js\n' + e + '```');
     }
 }
 
-function about(message) {
-    var embed = new MessageEmbed()
-        .setTitle('Tweeter by cyckl')
-        .setAuthor('About', 'https://github.com/cyckl/tweeter/raw/master/img/tweeter.png')
-        .setColor(0xFF453A)
-        .setDescription('Tweeter is a mock Twitter embed generator for Discord.')
-        .addField('Version', version, true)
-        .addField('Build date', '2020-04-12', true)
-        .setFooter('This is alpha software. Please be patient!')
-        .setThumbnail('https://github.com/cyckl/tweeter/raw/master/img/tweeter.png');
-    // Send tweet and log
-    message.channel.send(embed);
-    console.log('[About] (' + message.author.tag + ') ' + 'About dialogue triggered.');
-}
-
+// Authenticate: this is reliant on argument 1
 client.login(token);
